08d6e6fac5b9574eae445a340e87f4a8b883cc52
aulloa <aulloa@i-technology.cl>
2019-04-05 10:55:58 -0300

    ddd

--- index.php
+++ index.php
@@ -11,6 +11,8 @@ $ip = $_SERVER['REMOTE_ADDR'];
 echo "<h2>Client IP Demo</h2>";
 echo "Your IP address : " . $ip;
 echo "<br>Your hostname : ". gethostbyaddr($ip) ;
+//echo '<img src="images/greenrose.jpg" />';
+echo ' <table border=3> <tr><td><h1>its image </h1><img src="greenrose.jpg" alt="loading" /><td><td>hello</td><tr></table>';
 ?>
 
  
