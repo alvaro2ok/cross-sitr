7f14c1b73783f2876a618d0ac73e3464e38ab3d7
aulloa <aulloa@i-technology.cl>
2019-04-05 10:05:50 -0300

    bb

--- index.php
+++ index.php
@@ -3,6 +3,9 @@
   <title>PHP Test</title>
  </head>
  <body>
- <?php echo '<p>Hello World</p>'; ?> 
+ <?php echo '<p>Hello World</p>';  
+ $my_current_ip=exec("ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1'");
+ echo $my_current_ip;
+ ?>
  </body>
 </html>
